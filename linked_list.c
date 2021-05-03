0e3ddb243d553ad3c26dce9dbe897f81d1a4a285
Zel9689 <57132523+Zel9689@users.noreply.github.com>
2021-05-03 18:33:28 +0800

    list的頭放一個空節點的作法
    記得linked_list.h標頭檔也要載

--- linked_list.c
+++ linked_list.c
@@ -1,20 +1,22 @@
-// 已知bug，insert和delete不能用index 0，但我不知道怎麼辦
+// 在list的index0放值為0的節點，這樣對index0修改時，函式內的處理是一樣的流程
 // 不知道如何處理超出範圍的index
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct List List;
+#include "linked_list.h"
+
 struct List
 {
     int num;
     List *next;
 };
-List *search(List *, int);
-void append(List *, int);
-int insert(List *, int, int);
-void delete (List *, int);
-void pop(List *);
-void free_list(List *);
 
+List * list_init()
+{
+    List *null_head = malloc(sizeof(List));
+    null_head->num = 0;
+    null_head->next = NULL;
+    return null_head;
+}
 void append(List *list, int num)
 {
     List *current = list;
@@ -48,7 +50,7 @@ int insert(List *list, int index, int num)
     current->next = tmp; // 新的鍊結的next指向被插入的鍊結
 }
 
-void delete (List *list, int index)
+void del(List *list, int index)
 {
     List *current = search(list, index - 1); // 取目標index的前一個鍊結
     // if(current == 1) {
@@ -70,14 +72,14 @@ void pop(List *list)
         }
     }
     tmp->next = NULL; // 把倒數第二個鍊結的next，指向NULL
-
 }
 
-List *search(List *list, int index)
+List * search(List *list, int index)
 {
+    index = index + 1;
     List *target = list;
     for(int i = 0; i < index; i++) {
-        target = list->next;
+        target = target->next;
         // if(target == NULL) {
         //     return NULL; // 代表index超出範圍了
         // }
@@ -87,7 +89,7 @@ List *search(List *list, int index)
 
 void print_list(List *list)
 {
-    int count = 0;
+    int count = -1;
     List *current = list;
     while(1) {
         printf("index: %d, num: %d\n", count, current->num);
@@ -115,9 +117,8 @@ void free_list(List *list)
 
 int main()
 {
-    List *a = malloc(sizeof(List)); // create a list named "a"
-    a->num = 500;
-    a->next = NULL;
+    List *a = list_init(); // create a list named "a"
+    insert(a, 0, 500);
     printf("#### 存了第一筆資料:500 ####\n");
     print_list(a);
 
@@ -125,8 +126,8 @@ int main()
     printf("#### append一筆資料:24 ####\n");
     print_list(a);
 
-    insert(a, 1, 300);
-    printf("#### insert一筆資料:300到index1的地方 ####\n");
+    insert(a, 0, 300);
+    printf("#### insert一筆資料:300到index0的地方 ####\n");
     print_list(a);
 
     pop(a);
@@ -137,33 +138,75 @@ int main()
     printf("#### append一筆資料:100 ####\n");
     print_list(a);
 
-    delete(a, 1);
-    printf("#### delete一筆在index1的資料 ####\n");
+    append(a, 120);
+    printf("#### append一筆資料:120 ####\n");
     print_list(a);
+
+    append(a, 130);
+    printf("#### append一筆資料:130 ####\n");
+    print_list(a);
+
+    del(a, 0);
+    printf("#### delete一筆在index0的資料 ####\n");
+    print_list(a);
+
+    del(a, 2);
+    printf("#### delete一筆在index2的資料 ####\n");
+    print_list(a);
+
+    printf("\nSearch index:1的num為%d\n", search(a, 1)->num);
 
     free_list(a);
     return 0;
 }
 
+/* 輸出結果 */
 /*
-輸出結果：
 #### 存了第一筆資料:500 ####
+index: -1, num: 0
 index: 0, num: 500
 #### append一筆資料:24 ####
+index: -1, num: 0
 index: 0, num: 500
 index: 1, num: 24
-#### insert一筆資料:300到index1的地方 ####
-index: 0, num: 500
-index: 1, num: 300
+#### insert一筆資料:300到index0的地方 ####
+index: -1, num: 0
+index: 0, num: 300
+index: 1, num: 500
 index: 2, num: 24
 #### pop尾端一筆資料 ####
-index: 0, num: 500
-index: 1, num: 300
+index: -1, num: 0
+index: 0, num: 300
+index: 1, num: 500
 #### append一筆資料:100 ####
-index: 0, num: 500
-index: 1, num: 300
+index: -1, num: 0
+index: 0, num: 300
+index: 1, num: 500
 index: 2, num: 100
-#### delete一筆在index1的資料 ####
+#### append一筆資料:120 ####
+index: -1, num: 0
+index: 0, num: 300
+index: 1, num: 500
+index: 2, num: 100
+index: 3, num: 120
+#### append一筆資料:130 ####
+index: -1, num: 0
+index: 0, num: 300
+index: 1, num: 500
+index: 2, num: 100
+index: 3, num: 120
+index: 4, num: 130
+#### delete一筆在index0的資料 ####
+index: -1, num: 0
 index: 0, num: 500
 index: 1, num: 100
+index: 2, num: 120
+index: 3, num: 130
+#### delete一筆在index2的資料 ####
+index: -1, num: 0
+index: 0, num: 500
+index: 1, num: 100
+index: 2, num: 130
+
+Search index:1的num為100
 */
