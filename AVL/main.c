3f8ab594a391021896ab1ca9db0e95869849e9e3
zel <fishxd1096@gmail.com>
2021-10-03 01:39:19 +0800

    AVL主程式 和 AVL vs BST chart

--- AVL/main.c
+++ AVL/main.c
@@ -1,11 +1,14 @@
+/* !!編譯時要Define AVL才會啟用AVL的程式碼!! */
 #include <stdio.h>
 #include <stdlib.h>
 #include "../binary_tree/binary_tree.h"
 #include "../binary_tree/binary_tree_2dp.h"
+#include <time.h>
 
 int main()
 {
-    Tree_T* t = init(5);
+    for(int i=0; i<2; i++){
+    Tree_T *t = init(5);
     binary_tree_2dp(t);
     insert(t, -5);
     binary_tree_2dp(t);
@@ -19,10 +22,12 @@ int main()
     binary_tree_2dp(t);
     insert(t, -1);
     binary_tree_2dp(t);
-    insert(t, -5);
-    binary_tree_2dp(t);
+    printf("刪除node:7，AVL state:%d\n", i);
     del(t, 7);
     binary_tree_2dp(t);
-
+    sort(t);
+    printf("\n\n");
+    enable_AVL();
+    }
     return 0;
 }
