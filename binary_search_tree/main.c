d3b0fabf3313adb1c650ea01046e9710771b8ba9
zelus <fishxd1096@gmail.com>
2022-04-28 02:57:42 +0800

    修正binary_search_tree example沒有更新header file路徑的bug

--- binary_search_tree/main.c
+++ binary_search_tree/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../binary_tree/binary_tree.h"
-#include "../binary_tree/binary_tree_2dp.h"
+#include "../lib/binary_tree/binary_tree.h"
+#include "../lib/binary_tree/binary_tree_2dp.h"
 
 typedef struct Member Member_T;
 struct Member{
